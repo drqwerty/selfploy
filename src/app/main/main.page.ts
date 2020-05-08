04473d6a1e0c01d94eea764d04332aba38b8631b
Dani <dan7.9@hotmail.es>
2020-05-08 20:12:43 +0100

    Main page - style

---
+++ src/app/main/main.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage {
+
+  constructor() { }
+
+  continueWithEmail() {
+  }
+
+  continueWithGoogle() {
+  }
+
+  continueWithFacebook() {
+  }
+
+}
